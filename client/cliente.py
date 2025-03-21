ab24b563be151bc42dc7ad06717fe01b1f555d0e
Clara Almeida <gomes.almeida@academico.ifpb.edu.br>
2025-03-21 09:00:07 -0300

    :books: add codigo das mensagens de sucesso

--- client/cliente.py
+++ client/cliente.py
@@ -25,7 +25,9 @@ class Cliente:
         codigo, msg = resposta.split("|")
         if codigo.isdigit():
             codigo = int(codigo)
-            if codigo in [401, 411]:
+            if codigo in [200,211,221,231,241,251,261,271]:
+                print(f'Sucesso ao {msg}!')
+            elif codigo in [401, 411]:
                 print("Você já está logado.")
             elif codigo == 402:
                 print("Este CPF já está em uso. Talvez você já tenha se cadastrado anteriormente.")
@@ -35,39 +37,7 @@ class Cliente:
                 print("Dados com formato inválido")
             else:
                 print(msg.strip(" "))
-        #     # Cadastro
-        #     if codigo == 200:
-        #         print("Hóspede cadastrado com sucesso")
-        #     elif codigo == 401:
-        #         print("Hóspede já está cadastrado.")
-        #     elif codigo == 402:
-        #         print("Telefone com formato inválido")
-        #     elif codigo == 403:
-        #         print('Cpf Inválido')
-
-        #     # Login
-        #     elif codigo == 211:
-        #         print("Hóspede logado com sucesso!")
-        #     elif codigo == 411:
-        #         print("Hóspede já esta logado.")
-        #     elif codigo == 412:
-        #         print("Cpf ou senha incorretos.")
-        #     elif codigo == 201:
-        #         print("", msg.strip())
-        #     elif codigo == 202:
-        #         listaquarto(msg.strip())
-        #     elif codigo == 400:
-        #         print("Reserva não encontrada", msg.strip())
-        #     elif codigo == 401:
-        #         print("Acesso negado:", msg.strip())
-        #     elif codigo == 409:
-        #         print("Opção não é válida", msg.strip())
-        #     elif codigo == 500:
-        #         print("Erro interno do servidor:", msg.strip())
-        #     else:
-        #         print("Código desconhecido:", msg.strip())
-        # else:
-        #     print("Resposta do servidor:", resposta)
+       
 
     def menu(self):
         while True:
@@ -96,9 +66,6 @@ class Cliente:
                 self.enviar_requisicao(f"LOGIN|{cpf}|{senha}")
 
             elif opcao == "3":
-                """
-                Fazer verificaçoes no cliente (nao exclui as do servidor)
-                """
                 data_entrada = input("Data de entrada (ex: 15/03/2025): ").strip()
                 data_saida = input("Data de saída (ex: 16/03/2025): ").strip()
                 num_quarto = input("Número do quarto: ").strip()
@@ -112,10 +79,9 @@ class Cliente:
                 self.enviar_requisicao(f"CANCELAR|{num_quarto}|{data_entrada}")
 
             elif opcao == "5":
-                # cpf = input("Digite seu CPF: ")
                 ano = input("Digite o ano da reserva: ").strip()
                 self.enviar_requisicao(f"CONSULTAR|{ano}")
-                # self.enviar_requisicao(f"CONSULTAR {cpf} {ano}")
+               
             elif opcao == "6":
                 self.enviar_requisicao(f"LISTAR")
 
